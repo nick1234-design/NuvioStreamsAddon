97bd1f3882781e97d4072b74556b3a623a8e90cb
nick1234-design <Nicholasborgards1@gmail.com>
2026-09-14 12:54:51 -0700

    Update types.js

--- providers/febbox/types.js
+++ providers/febbox/types.js
@@ -1,24 +1,32 @@
 'use strict';
 
-function createStream({
-  url,
-  title = 'FebBox',
-  quality = 0,
-  size = 0,
-  codec = '',
-}) {
-  return {
-    url,
-    title,
-    quality,
-    size,
-    codec,
-    behaviorHints: {
-      bingeGroup: 'febbox',
-    },
-  };
+/**
+ * @typedef {Object} FebBoxFile
+ * @property {string} fid
+ * @property {string} name
+ * @property {boolean} isDir
+ * @property {string} [size]
+ *
+ * @typedef {Object} FebBoxQualityLink
+ * @property {string} url
+ * @property {string} quality
+ * @property {string} [name]
+ * @property {string} [size]
+ * @property {string} [speed]
+ *
+ * @typedef {'AUTH_INVALID'|'QUOTA_EXCEEDED'|'UPSTREAM_TIMEOUT'|'UPSTREAM_ERROR'|'NOT_FOUND'|'RATE_LIMITED'} FebBoxErrorCode
+ */
+
+class FebBoxError extends Error {
+  /**
+   * @param {string} message
+   * @param {FebBoxErrorCode} code
+   */
+  constructor(message, code) {
+    super(message);
+    this.name = 'FebBoxError';
+    this.code = code || 'UPSTREAM_ERROR';
+  }
 }
 
-module.exports = {
-  createStream,
-};
+module.exports = { FebBoxError };
